9009770bf1785037c3fc7ecb7a0be129d073723b
erdem abat <erdemabat@hotmail.com>
2024-08-23 19:28:30 +0300

    Add project files.

---
+++ LoginOtpCodeVerification/IService1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LoginOtpCodeVerification
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        OtpModel CheckOtp(ValidationModel validationModel);
+
+    }
+
+
+    [DataContract]
+    public class ValidationModel
+    {
+        string email = string.Empty;
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+
+    [DataContract]
+    public class OtpModel
+    {
+        string otpCode = string.Empty;
+        bool status = false;
+
+        [DataMember]
+        public string OtpCode
+        {
+            get { return otpCode; }
+            set { otpCode = value; }
+        }
+
+        [DataMember]
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}
---
+++ LoginOtpCodeVerification/Service1.svc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+
+namespace LoginOtpCodeVerification
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public OtpModel CheckOtp(ValidationModel validationModel)
+        {
+            Random r = new Random();
+            string otp = r.Next(10001,99999).ToString();
+
+            try
+            {
+                using (MailMessage mm = new MailMessage("notifyproject101@hotmail.com", validationModel.Email))
+                {
+                    mm.Subject = "Otp Onay";
+                    string msg = "Otp kodunuz: " + otp;
+                    mm.Body = msg;
+                    mm.IsBodyHtml = false;
+
+                    using (SmtpClient smtpClient = new SmtpClient())
+                    {
+                        smtpClient.Host = "smtp-mail.outlook.com";
+                        smtpClient.EnableSsl = true;
+                        smtpClient.Port = 587;
+                        smtpClient.UseDefaultCredentials = false;
+                        smtpClient.Credentials = new NetworkCredential("notifyproject101@hotmail.com", "Erdemabat1.");
+
+                        smtpClient.Send(mm);
+                    }
+                }
+            }
+            catch (SmtpException smtpEx)
+            {
+                Console.WriteLine("SMTP Exception: " + smtpEx.Message);
+                Console.WriteLine("Status Code: " + smtpEx.StatusCode);
+                if (smtpEx.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception: " + smtpEx.InnerException.Message);
+                }
+                return new OtpModel
+                {
+                    OtpCode = otp,
+                    Status = false
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
+                }
+                return new OtpModel
+                {
+                    OtpCode = otp,
+                    Status = false
+                };
+            }
+
+            return new OtpModel
+            {
+                OtpCode = otp,
+                Status = true
+            };
+        }
+
+    }
+}
